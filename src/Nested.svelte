3bfaa387d1d4f25305b33236d8852cda053cc0c2
Tommy Melançon-Roy <tmelanconroy@gmail.com>
2023-10-11 04:00:21 -0400

    create svelte project to learn with official tutorials, i'm at part 1 - reactivity

---
+++ src/Nested.svelte
@@ -0,0 +1,13 @@
+<script>
+    let isItHtml ='<em>HTML</strong>'
+</script>
+
+<h3>This is an nested component {@html isItHtml}</h3>
+
+<style>
+    h3{
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Nested from "../Nested.svelte";
+
+    let userName = 'Tommy'
+    const src = "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg"
+    const description = "It's a man"
+</script>
+
+<h1>Hello new {userName.toUpperCase()} !</h1>
+
+<img {src} alt="{description} with a blue jean">
+
+<Nested/>
+
+<style>
+    :root{
+        background-color: rgb(33, 33, 33);
+    }
+
+    h1{
+        color: white;
+    }
+</style>
+
+
+<!-- PART 1 - REACTIVITY -->
